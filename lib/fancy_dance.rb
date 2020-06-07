1126c9f4001f3b0a6d5607a6e3404db6661fdaa5
Phil Humphrey <phumphrey588@yahoo.com>
2020-06-07 08:05:26 +0000

    Automatically backed up by Learn

--- lib/fancy_dance.rb
+++ lib/fancy_dance.rb
@@ -19,4 +19,9 @@ module FancyDance
         end
       end
       
+      module ClassMethods
+        
+        def metadata
+          "This class produces objects that love to dance."
+      
       
